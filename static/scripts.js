ed384cac49cd993f089efa09afa89dabcf3ed12c
jcardoza21 <jcardoza21@hotmail.com>
2021-05-25 05:06:00 +0000

    reparado

--- static/scripts.js
+++ static/scripts.js
@@ -4,15 +4,8 @@ function example1(mecanismo)
     if (mecanismo == conduccion)
     {
         document.getElementById('ejemplo1-text').innerHTML = 
-        <ol>
-        <li>La mayor parte de la conducción ocurre en la superficie de la Tierra.
-        Durante el día, la radiación solar calienta el suelo y el agua, que luego calientan el aire que los toca.
-        El aire no es un buen conductor de energía térmica porque las partículas de los gases que componen la atmósfera están muy separadas.
-        </li>
-        </ol>
-        ;
-        
-        document.getElementById('ejemplo1-img').src='https://pbslm-contrib.s3.amazonaws.com/WGBH/conv16/conv16-int-thermalenergy/images/earth_science/earth_2.gif';
+        "<ol><li>La mayor parte de la conducción ocurre en la superficie de la Tierra. Durante el día, la radiación solar calienta el suelo y el agua, que luego calientan el aire que los toca. El aire no es un buen conductor de energía térmica porque las partículas de los gases que componen la atmósfera están muy separadas.</li></ol>";
+        document.getElementById('ejemplo1-img').src="https://pbslm-contrib.s3.amazonaws.com/WGBH/conv16/conv16-int-thermalenergy/images/earth_science/earth_2.gif";
     }
     else 
     {
@@ -31,3 +24,4 @@ function example1(mecanismo)
         }
     }
 }
+
