b2dd1a44b8f40253f2bdf7cff1c7233010a00c06
jcardoza21 <jcardoza21@hotmail.com>
2021-05-25 18:00:21 +0000

    terminado

--- static/scripts-examples.js
+++ static/scripts-examples.js
@@ -30,3 +30,40 @@ function example2_conduccion()
         document.querySelector('#ejemplo2-img').src = "https://pbslm-contrib.s3.amazonaws.com/WGBH/conv16/conv16-int-thermalenergy/images/physical_science/fire2.gif";
 }
 
+function example2_conveccion()
+{
+        document.querySelector('#aux_2').innerHTML = "";
+        document.querySelector('#ejemplo2-text').innerHTML = "<ol><li>A medida que el aire alrededor de la fogata se calienta y se vuelve menos denso, se eleva y se mueve hacia arriba, creando una corriente de convección que transfiere calor. Puede ver esta convección natural cuando el humo se eleva desde el fuego.</li></ol>";
+        document.querySelector('#ejemplo2-img').src = "https://pbslm-contrib.s3.amazonaws.com/WGBH/conv16/conv16-int-thermalenergy/images/physical_science/fire3.gif";
+}
+
+function example2_radiacion()
+{
+        document.querySelector('#aux_2').innerHTML = "";
+        document.querySelector('#ejemplo2-text').innerHTML = "<ol><li>La energía térmica se irradia de una fogata. El calor que siente al sentarse cerca de una fogata se debe principalmente a la transferencia de energía térmica por radiación.</li><li>El malvavisco que se mantiene cerca del fuego también obtiene energía térmica de la radiación, que tuesta el malvavisco.</li></ol>";
+        document.querySelector('#ejemplo2-img').src = "https://pbslm-contrib.s3.amazonaws.com/WGBH/conv16/conv16-int-thermalenergy/images/physical_science/fire4.gif";
+}
+
+// Cambiar el texto y la imagen del TERCER ejemplo para cada mecanismo de TdeQ
+function example3_conduccion()
+{
+        document.querySelector('#aux_3').innerHTML = "";
+        document.querySelector('#ejemplo3-text').innerHTML = "<ol><li>La conducción tiene lugar en la superficie de la piel, ya que la energía térmica se transfiere desde su cuerpo al aire o cualquier cosa que esté en contacto con su piel.</li><li>Es posible que prefiera pararse sobre un piso de baldosas en lugar de una alfombra. Las baldosas son un mejor conductor de energía térmica, por lo que es más fácil que el calor fluya de los pies a las baldosas. Esta conducción hace que el azulejo se sienta fresco.</li></ol>";
+        document.querySelector('#ejemplo3-img').src = "https://pbslm-contrib.s3.amazonaws.com/WGBH/conv16/conv16-int-thermalenergy/images/life_science/person_2.gif";
+}
+
+function example3_conveccion()
+{
+        document.querySelector('#aux_3').innerHTML = "";
+        document.querySelector('#ejemplo3-text').innerHTML = "<ol><li>Cuando su cuerpo está demasiado caliente, aumenta el flujo sanguíneo a su piel, donde ocurre la pérdida de calor.</li><li>Es posible que prefiera estar en la planta baja en lugar de arriba, porque el aire caliente se eleva por convección. Donde el aire es más frío en la planta baja, se transferirá más energía térmica de su cuerpo al aire.</li><li>Un ventilador aumenta el flujo de aire en una habitación y puede ayudar a aumentar la transferencia de energía térmica de su cuerpo al aire. El aire en movimiento aumenta la tasa de pérdida de calor por la evaporación del sudor.</li></ol>";
+        document.querySelector('#ejemplo3-img').src = "https://pbslm-contrib.s3.amazonaws.com/WGBH/conv16/conv16-int-thermalenergy/images/life_science/person_3.gif";
+}
+
+function example3_radiacion()
+{
+        document.querySelector('#aux_3').innerHTML = "";
+        document.querySelector('#ejemplo3-text').innerHTML = "<ol><li>La radiación del Sol transfiere energía térmica a una persona que se encuentra a la luz del sol. Permanecer en áreas con sombra puede ayudarlo a mantenerse fresco en un día caluroso.</li><li>Tu cuerpo también pierde energía térmica al medio ambiente por radiación.</li></ol>";
+        document.querySelector('#ejemplo3-img').src = "https://pbslm-contrib.s3.amazonaws.com/WGBH/conv16/conv16-int-thermalenergy/images/life_science/person_4.gif";
+}
+
+
