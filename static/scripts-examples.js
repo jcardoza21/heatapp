6b6ed639c291cd91543ee68090c5efd1106ab7f0
jcardoza21 <jcardoza21@hotmail.com>
2021-05-25 16:03:55 +0000

    ejemplos_reparado

---
+++ static/scripts-examples.js
@@ -0,0 +1,32 @@
+// Javascript para examples.html
+// Cambiar el texto y la imagen del PRIMER ejemplo para cada mecanismo de TdeQ
+function example1_conduccion()
+{
+        document.querySelector('#aux_1').innerHTML = "";
+        document.querySelector('#ejemplo1-text').innerHTML = "<ol><li>La mayor parte de la conducción ocurre en la superficie de la Tierra. Durante el día, la radiación solar calienta el suelo y el agua, que luego calientan el aire que los toca. El aire no es un buen conductor de energía térmica porque las partículas de los gases que componen la atmósfera están muy separadas.</li></ol>";
+        document.querySelector('#ejemplo1-img').src = "https://pbslm-contrib.s3.amazonaws.com/WGBH/conv16/conv16-int-thermalenergy/images/earth_science/earth_2.gif";
+}
+
+function example1_conveccion()
+{
+        document.querySelector('#aux_1').innerHTML = "";
+        document.querySelector('#ejemplo1-text').innerHTML = "<ol><li>El Sol calienta la Tierra de manera desigual, y las áreas cercanas al Ecuador se calientan más intensamente que las áreas polares. Las corrientes de convección en la atmósfera distribuyen la energía térmica alrededor de la Tierra.</li><li>La circulación oceánica global también distribuye energía térmica alrededor de la Tierra.</li><li>La convección del manto transporta energía térmica desde el interior de la Tierra a su superficie e impulsa la tectónica de placas, provocando fenómenos como terremotos y volcanes.</li></ol>";
+        document.querySelector('#ejemplo1-img').src = "https://pbslm-contrib.s3.amazonaws.com/WGBH/conv16/conv16-int-thermalenergy/images/earth_science/earth_3.gif";
+}
+
+function example1_radiacion()
+{
+        document.querySelector('#aux_1').innerHTML = "";
+        document.querySelector('#ejemplo1-text').innerHTML = "<ol><li>El Sol es la principal fuente de energía térmica de la Tierra. La radiación del Sol viaja a través del espacio y calienta la atmósfera y la superficie de la Tierra.</li><li>Parte de la radiación solar es reflejada hacia el espacio por las nubes y la superficie de la Tierra.</li><li>La Tierra también emite energía térmica al espacio.</li><li>Los gases de efecto invernadero atrapan algo de energía térmica en la atmósfera.</li></ol>";
+        document.querySelector('#ejemplo1-img').src = "https://pbslm-contrib.s3.amazonaws.com/WGBH/conv16/conv16-int-thermalenergy/images/earth_science/earth_4.gif";
+}
+
+
+// Cambiar el texto y la imagen del SEGUNDO ejemplo para cada mecanismo de TdeQ
+function example2_conduccion()
+{
+        document.querySelector('#aux_2').innerHTML = "";
+        document.querySelector('#ejemplo2-text').innerHTML = "<ol><li>Una varilla de metal no sería una buena opción para asar malvaviscos porque el metal es un buen conductor de energía térmica. ¡Rápidamente se volvería demasiado caliente para sostenerlo!</li><li>Una ramita de árbol sería una mejor opción, porque la madera es un mal conductor de energía térmica.</li></ol>";
+        document.querySelector('#ejemplo2-img').src = "https://pbslm-contrib.s3.amazonaws.com/WGBH/conv16/conv16-int-thermalenergy/images/physical_science/fire2.gif";
+}
+
